bc4ef60bfec6d80ecc93797e67f30b2fcd590e9b
Ken Kousen <ken.kousen@kousenit.com>
2021-09-07 04:30:48 -0400

    Change eager to lazy fetch

--- src/main/java/com/kousenit/sakila/dao/CityRepository.java
+++ src/main/java/com/kousenit/sakila/dao/CityRepository.java
@@ -2,7 +2,6 @@ package com.kousenit.sakila.dao;
 
 import com.kousenit.sakila.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
--- src/main/java/com/kousenit/sakila/entities/City.java
+++ src/main/java/com/kousenit/sakila/entities/City.java
@@ -15,7 +15,7 @@ public class City {
     @Column(name = "city")
     private String name;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "country_id")
     private Country country;
 
