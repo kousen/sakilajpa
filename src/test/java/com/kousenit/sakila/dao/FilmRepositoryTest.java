7de1230e7bec00e29b19eab493622b6adbb8b883
Ken Kousen <ken.kousen@kousenit.com>
2022-07-03 14:52:45 -0400

    Add assertions to stored procedure call

--- src/test/java/com/kousenit/sakila/dao/FilmRepositoryTest.java
+++ src/test/java/com/kousenit/sakila/dao/FilmRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.kousenit.sakila.dao;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -24,13 +28,13 @@ class FilmRepositoryTest {
     void invokeFilmInStockStoredProcedureUsingProcName() {
         Integer stock = repository.numberOfFilmsInStock(1, 1);
         System.out.println("There is/are " + stock + " copies of film 1 at store 1");
+        assertThat(stock).isEqualTo(4);
     }
 
     @Test @Disabled("Probable bug calling stored procedure")
     void invokeFilmInStockStoredProcedureUsingName() {
         Integer stock = repository.filmsInStockAtStore(1, 1);
         System.out.println("There is/are " + stock + " copies of film 1 at store 1");
+        assertThat(stock).isEqualTo(4);
     }
-
-
 }
